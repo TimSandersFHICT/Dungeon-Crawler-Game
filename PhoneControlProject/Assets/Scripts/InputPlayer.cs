24a60ce42f5a770b95c833977b4eb05ec8e22876
Tim S <tim.sanders@student.fontys.nl>
2018-09-13 13:43:13 +0200

    First Commit
    - Added Random Dungeon Generation (small bug with joined rooms)
    - Added mobile joystick controls

---
+++ PhoneControlProject/Assets/Scripts/InputPlayer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputPlayer : MonoBehaviour {
+
+    protected Joystick joystick;
+    protected Joybutton joybutton;
+
+    // Use this for initialization
+    void Start () {
+        joystick = FindObjectOfType<Joystick>();
+        joybutton = FindObjectOfType<Joybutton>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        var rigidbody = GetComponent<Rigidbody2D>();
+
+        rigidbody.velocity = new Vector2(joystick.Horizontal * 50, joystick.Vertical * 50);
+	}
+}
